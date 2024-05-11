e3f3ef1747074751d36ab4d8b6ec622207406b94
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 19:17:34 +0530

    Update loop.py

--- TitanXMusic/plugins/admins/loop.py
+++ TitanXMusic/plugins/admins/loop.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_loop, set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from TitanXMusic import app
+from TitanXMusic.utils.database import get_loop, set_loop
+from TitanXMusic.utils.decorators import AdminRightsCheck
+from TitanXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
