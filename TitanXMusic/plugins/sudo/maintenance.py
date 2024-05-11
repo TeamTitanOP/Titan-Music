d32a26179026354fbfb21924155f9337cef676ac
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 18:25:00 +0530

    Update maintenance.py

--- TitanXMusic/plugins/sudo/maintenance.py
+++ TitanXMusic/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from TitanXMusic import app
+from TitanXMusic.misc import SUDOERS
+from TitanXMusic.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
