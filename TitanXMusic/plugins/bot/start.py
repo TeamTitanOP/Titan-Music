e4111b22fc7075d71151d514a7e7202c904f2b21
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 18:53:06 +0530

    Update start.py

--- TitanXMusic/plugins/bot/start.py
+++ TitanXMusic/plugins/bot/start.py
@@ -6,10 +6,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import _boot_
-from AnonXMusic.plugins.sudo.sudoers import sudoers_list
-from AnonXMusic.utils.database import (
+from TitanXMusic import app
+from TitanXMusic.misc import _boot_
+from TitanXMusic.plugins.sudo.sudoers import sudoers_list
+from TitanXMusic.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -17,9 +17,9 @@ from AnonXMusic.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from AnonXMusic.utils.decorators.language import LanguageStart
-from AnonXMusic.utils.formatters import get_readable_time
-from AnonXMusic.utils.inline import help_pannel, private_panel, start_panel
+from TitanXMusic.utils.decorators.language import LanguageStart
+from TitanXMusic.utils.formatters import get_readable_time
+from TitanXMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
