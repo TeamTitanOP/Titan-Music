d223f1c69af6d68aa7e7ef5a29370be342ddebc3
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 18:44:35 +0530

    Update channel.py

--- TitanXMusic/plugins/play/channel.py
+++ TitanXMusic/plugins/play/channel.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import set_cmode
-from AnonXMusic.utils.decorators.admins import AdminActual
+from TitanXMusic import app
+from TitanXMusic.utils.database import set_cmode
+from TitanXMusic.utils.decorators.admins import AdminActual
 from config import BANNED_USERS
 
 
