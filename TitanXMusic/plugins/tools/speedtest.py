db9c95b07402893ba6307e852fdd0f96fa174de0
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 17:43:14 +0530

    Update speedtest.py

--- TitanXMusic/plugins/tools/speedtest.py
+++ TitanXMusic/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.decorators.language import language
+from TitanXMusic import app
+from TitanXMusic.misc import SUDOERS
+from TitanXMusic.utils.decorators.language import language
 
 
 def testspeed(m, _):
