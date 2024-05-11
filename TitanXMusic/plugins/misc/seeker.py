cba4bb18672eaa7e96bd5ff8673c9c256b9421a8
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 18:46:45 +0530

    Update seeker.py

--- TitanXMusic/plugins/misc/seeker.py
+++ TitanXMusic/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_active_chats, is_music_playing
+from TitanXMusic.misc import db
+from TitanXMusic.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
