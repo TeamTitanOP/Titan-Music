a558ba29a6ce5dc67c4fa424b6e29bab37bf4ad6
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 18:45:43 +0530

    Update watcher.py

--- TitanXMusic/plugins/misc/watcher.py
+++ TitanXMusic/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
+from TitanXMusic import app
+from TitanXMusic.core.call import Titany
 
 welcome = 20
 close = 30
@@ -11,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await Anony.stop_stream_force(message.chat.id)
+    await Titany.stop_stream_force(message.chat.id)
