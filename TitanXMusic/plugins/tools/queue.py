0b1f9a240ec15c488fe81569e1ec1a099de54d73
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 17:58:26 +0530

    Update queue.py

--- TitanXMusic/plugins/tools/queue.py
+++ TitanXMusic/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AnonyBin, get_channeplayCB, seconds_to_min
-from AnonXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline import queue_back_markup, queue_markup
+from TitanXMusic import app
+from TitanXMusic.misc import db
+from TitanXMusic.utils import TitanyBin, get_channeplayCB, seconds_to_min
+from TitanXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
+from TitanXMusic.utils.decorators.language import language, languageCB
+from TitanXMusic.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
@@ -176,7 +176,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
             return await CallbackQuery.edit_message_text(msg, reply_markup=buttons)
         if "✨" in msg:
             msg = msg.replace("✨", "")
-        link = await AnonyBin(msg)
+        link = await TitanyBin(msg)
         med = InputMediaPhoto(media=link, caption=_["queue_3"].format(link))
         await CallbackQuery.edit_message_media(media=med, reply_markup=buttons)
     else:
