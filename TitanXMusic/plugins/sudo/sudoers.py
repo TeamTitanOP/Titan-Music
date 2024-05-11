f198c9eb08a2ccc4d79907eafd9f0ffedc12ede9
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 18:20:47 +0530

    Update sudoers.py

--- TitanXMusic/plugins/sudo/sudoers.py
+++ TitanXMusic/plugins/sudo/sudoers.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_sudo, remove_sudo
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
-from AnonXMusic.utils.inline import close_markup
+from TitanXMusic import app
+from TitanXMusic.misc import SUDOERS
+from TitanXMusic.utils.database import add_sudo, remove_sudo
+from TitanXMusic.utils.decorators.language import language
+from TitanXMusic.utils.extraction import extract_user
+from TitanXMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
