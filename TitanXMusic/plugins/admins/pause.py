468ac8c3db3ba67b913a3ef1902655affdccdf3c
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 19:16:32 +0530

    Update pause.py

--- TitanXMusic/plugins/admins/pause.py
+++ TitanXMusic/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_off
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from TitanXMusic import app
+from TitanXMusic.core.call import Titany 
+from TitanXMusic.utils.database import is_music_playing, music_off
+from TitanXMusic.utils.decorators import AdminRightsCheck
+from TitanXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Anony.pause_stream(chat_id)
+    await Titany.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
