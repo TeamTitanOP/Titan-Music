eee03c708a3d90292e2f18922a4ad678a69d5dee
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 17:36:44 +0530

    Update admins.py

--- TitanXMusic/utils/decorators/admins.py
+++ TitanXMusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS, db
-from AnonXMusic.utils.database import (
+from TitanXMusic import app
+from TitanXMusic.misc import SUDOERS, db
+from TitanXMusic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
