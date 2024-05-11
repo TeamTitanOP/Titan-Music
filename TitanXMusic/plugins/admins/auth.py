56ed66bfa2bec46ce9c64403c78d146b44d18d90
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 19:39:10 +0530

    Update auth.py

--- TitanXMusic/plugins/admins/auth.py
+++ TitanXMusic/plugins/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import extract_user, int_to_alpha
-from AnonXMusic.utils.database import (
+from TitanXMusic import app
+from TitanXMusic.utils import extract_user, int_to_alpha
+from TitanXMusic.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from AnonXMusic.utils.decorators import AdminActual, language
-from AnonXMusic.utils.inline import close_markup
+from TitanXMusic.utils.decorators import AdminActual, language
+from TitanXMusic.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
