cbd4d365310a20fb57e31fd5ab20d2d83e88b3e1
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 18:48:51 +0530

    Update broadcast.py

--- TitanXMusic/plugins/misc/broadcast.py
+++ TitanXMusic/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from TitanXMusic import app
+from TitanXMusic.misc import SUDOERS
+from TitanXMusic.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.formatters import alpha_to_int
+from TitanXMusic.utils.decorators.language import language
+from TitanXMusic.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
@@ -117,7 +117,7 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_5"])
         text = _["broad_6"]
-        from AnonXMusic.core.userbot import assistants
+        from TitanXMusic.core.userbot import assistants
 
         for num in assistants:
             sent = 0
