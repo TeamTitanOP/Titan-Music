48e6c328a7efa98ff6e30373562f738cec8cdcae
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 18:57:08 +0530

    Update help.py

--- TitanXMusic/plugins/bot/help.py
+++ TitanXMusic/plugins/bot/help.py
@@ -3,11 +3,11 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import help_pannel
-from AnonXMusic.utils.database import get_lang
-from AnonXMusic.utils.decorators.language import LanguageStart, languageCB
-from AnonXMusic.utils.inline.help import help_back_markup, private_help_panel
+from TitanXMusic import app
+from TitanXMusic.utils import help_pannel
+from TitanXMusic.utils.database import get_lang
+from TitanXMusic.utils.decorators.language import LanguageStart, languageCB
+from TitanXMusic.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
