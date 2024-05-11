b54b3dfbba0b3d38db00b0944bc442de077be43c
ᴛᴇᴀᴍ ᴛɪᴛᴀɴ ᴏᴘ (•‿•) <anayajat2003@gmail.com>
2024-05-11 16:59:09 +0530

    Update queue.py

--- TitanXMusic/utils/stream/queue.py
+++ TitanXMusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from AnonXMusic.misc import db
-from AnonXMusic.utils.formatters import check_duration, seconds_to_min
+from TitanXMusic.misc import db
+from TitanXMusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
